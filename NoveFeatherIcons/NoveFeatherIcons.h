2f2d081a71cf75bdf65395d2771c07b6239fa843
Steve Gigou <steve@account.gigou.fr>
2019-10-22 14:37:23 +0200

    Create project

---
+++ NoveFeatherIcons/NoveFeatherIcons.h
@@ -0,0 +1,19 @@
+//
+//  NoveFeatherIcons.h
+//  NoveFeatherIcons
+//
+//  Created by Steve Gigou on 22/10/2019.
+//  Copyright © 2019 Novesoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NoveFeatherIcons.
+FOUNDATION_EXPORT double NoveFeatherIconsVersionNumber;
+
+//! Project version string for NoveFeatherIcons.
+FOUNDATION_EXPORT const unsigned char NoveFeatherIconsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NoveFeatherIcons/PublicHeader.h>
+
+
